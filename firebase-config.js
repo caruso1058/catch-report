ca53d91aff9233a287c6b865af42626a87eda1c0
Jules Caruso <49770101+caruso1058@users.noreply.github.com>
2026-05-07 23:48:51 -0700

    Enable Firebase project config

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,13 @@
-// Public Firebase web config goes here after you create the free Spark project.
-// This object is not a database password. Firestore security rules protect data.
-export const firebaseEnabled = false;
+// Public Firebase web config. This is not a database password.
+// Firestore security rules protect user catch data.
+export const firebaseEnabled = true;
 
 export const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  appId: "",
+  apiKey: "AIzaSyDc0BHfjmAbpzgcRxRotZ0LuO4IMHwqTwg",
+  authDomain: "catch-report-80cc8.firebaseapp.com",
+  projectId: "catch-report-80cc8",
+  storageBucket: "catch-report-80cc8.firebasestorage.app",
+  messagingSenderId: "327961036176",
+  appId: "1:327961036176:web:3427e072f65dd3dfcb9f15",
+  measurementId: "G-XKW9RKY4JE",
 };
